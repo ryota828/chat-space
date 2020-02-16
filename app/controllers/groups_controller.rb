9c040bb46180b8aca986418b5aafaea530c10b3a
ryota828 <ry0ta256@yahoo.co.jp>
2020-02-16 15:23:03 +0900

    サイドバーにグループ一覧を表示
    サイドバーにグループ一覧を表示。また、ペンマークのアイコンをクリックしたら、グループの新規作成画面に遷移するよう設定。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -25,7 +28,7 @@ class GroupsController < ApplicationController
       render :edit
     end
   end
-
+  
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
