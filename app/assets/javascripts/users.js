69a9374affafad203205a3385bbc1d6a0a2357aa
ryota828 <ry0ta256@yahoo.co.jp>
2020-03-08 17:35:17 +0900

    Update users.js
    非同期通信の結果を得て、HTMLを作成する
    作成したHTMLをビュー上に描画する
    エラー時の処理を行う

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,20 @@
 $(function() {
+  function  addUser(user){
+    let html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+              </div>
+              `
+    $("#user-search-result").append(html)
+  }
+  function  addNouser(){
+    let html = `
+               <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+               </div>`
+    $("#user-search-result").append(html)
+}
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -21,7 +37,7 @@ $(function() {
         }
       })
       .fail(function() {
-        console.log("失敗です");
+        alert("ユーザー検索に失敗しました");
       });
   });
 });
